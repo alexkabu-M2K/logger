c1da0e63efc3012daa059149185528a63a564a98
Alexkabu <alexkabu@outlook.com>
2025-08-02 11:19:21 -0300

    criando logica de cadastro

---
+++ scripts/register_aula_19.js
@@ -0,0 +1,18 @@
+
+var register = document.getElementById("register");
+
+register.addEventListener("click", function() {
+
+    var name = document.getElementById("name").value;
+    var user = document.getElementById("userRegister").value;
+    var password = document.getElementById("passwordRegister").value;
+
+    sessionStorage.setItem("name", name);
+    sessionStorage.setItem("user", user);
+    sessionStorage.setItem("password", password);
+
+    alert("Cadastro realizado com sucesso!");
+    // Redireciona para a página de login após o cadastro
+
+    window.location.href = "../pages/login_aula_19.html";
+});
