f8c061cde0f788bc38b2b7a6072af357d622750c
Alexkabu <alexkabu@outlook.com>
2025-08-03 12:37:44 -0300

    Alterações e Otimização do Código

--- scripts/allowed_aula_19.js
+++ scripts/allowed_aula_19.js
@@ -1,5 +1,9 @@
 
 var greeting = document.getElementById("greeting");
 
-greeting.innerText = `Olá, ${sessionStorage.getItem("name")}!`;
+var name = sessionStorage.getItem("name");
+
+greeting.innerText = "Olá, " + sessionStorage.getItem("name") + "!";
+
+// Exibe uma mensagem de saudação ao usuário após o login
 
--- scripts/register_aula_19.js
+++ scripts/register_aula_19.js
@@ -11,7 +11,9 @@ register.addEventListener("click", function() {
     sessionStorage.setItem("user", user);
     sessionStorage.setItem("password", password);
 
+    // Exibe uma mensagem de sucesso
     alert("Cadastro realizado com sucesso!");
+    
     // Redireciona para a página de login após o cadastro
 
     window.location.href = "../pages/login_aula_19.html";
